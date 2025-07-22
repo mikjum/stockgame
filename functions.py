f53505d3499980ad5dce5e281f43e89b0d45e408
Mikko <mikko.makelavaitilo@gmail.com>
2025-07-22 10:30:50 +0300

    added load data function

--- functions.py
+++ functions.py
@@ -1,1 +1,12 @@
+import json
 
+def load_data():
+    if DATA_FILE.exists():
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    else:
+        return {"cash": 10000, "portfolio": {}}
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
